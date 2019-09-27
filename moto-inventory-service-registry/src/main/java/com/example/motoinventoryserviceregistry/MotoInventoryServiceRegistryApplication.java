2e0132b73e4a7c8ead9015fc7c22b904e7b66aec
DevAaron <devaaronlee@gmail.com>
2019-09-27 10:02:30 -0400

    initial setup

---
+++ moto-inventory-service-registry/src/main/java/com/example/motoinventoryserviceregistry/MotoInventoryServiceRegistryApplication.java
@@ -0,0 +1,15 @@
+package com.example.motoinventoryserviceregistry;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+
+@SpringBootApplication
+@EnableEurekaServer
+public class MotoInventoryServiceRegistryApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MotoInventoryServiceRegistryApplication.class, args);
+	}
+
+}
